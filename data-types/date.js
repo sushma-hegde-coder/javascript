6c1c2eed69d0b8b69c489ed4882d6d211e6c39db
sushma-hegde-coder <35775913+sushma-hegde-coder@users.noreply.github.com>
2021-06-11 19:58:27 +0530

    Create date.js

---
+++ data-types/date.js
@@ -0,0 +1,4 @@
+ var lastloginDetails=new Date("2021-06-11T13:48:24.000Z").toLocaleDateString()
+  var lastloginDetails1=new Date("2021-06-11T13:48:24.000Z").toString()
+  var lastloginDetails2=new Date("2021-06-11T13:48:24.000Z").toDateString()
+  var lastloginDetails3=new Date("2021-06-11T13:48:24.000Z").toTimeString();
