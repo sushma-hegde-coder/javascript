a8ee2aed9d65db02da8085b4330b0b091916c1aa
sushma-hegde-coder <35775913+sushma-hegde-coder@users.noreply.github.com>
2021-06-11 20:20:33 +0530

    Update date.js

--- data-types/date.js
+++ data-types/date.js
@@ -2,3 +2,7 @@
   var lastloginDetails1=new Date("2021-06-11T13:48:24.000Z").toString()
   var lastloginDetails2=new Date("2021-06-11T13:48:24.000Z").toDateString()
   var lastloginDetails3=new Date("2021-06-11T13:48:24.000Z").toTimeString();
+ var lastloginDetails4=new Date("2021-06-11T13:48:24.000Z").toLocaleTimeString();
+
+ <Title level={4}>Your last login is {lastloginDetails} {lastloginDetails4}</Title>
+ <Title level={4}>Your last login was on {lastloginDetails2} {lastloginDetails4}</Title>
